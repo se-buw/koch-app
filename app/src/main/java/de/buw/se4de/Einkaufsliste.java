0de1a7e9a05bdc1957e53a058e84fed28ee73820
Anastasia Koslova <anastasia.koslova@uni-weimar.de>
2022-11-04 16:27:33 +0100

    Jetzt habe ich Zugriff auf eine csv Datei und kann reinschreiben

--- app/src/main/java/de/buw/se4de/Einkaufsliste.java
+++ app/src/main/java/de/buw/se4de/Einkaufsliste.java
@@ -1,31 +1,46 @@
 package de.buw.se4de;
-import java.io.IOException;
-import java.io.FileWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.BufferedReader;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 
 public class Einkaufsliste {
     String item;
+    private static final String LISTE = "liste.csv";
 
-    public static void main (String[] args){
-        String line = "";
-        String splitBy = ",";
-        try {
-            BufferedReader a = new BufferedReader(new FileReader("liste.csv"));
-            while ((line = a.readLine()) != null)   //returns a Boolean value
-            {
-                System.out.println(line);
-            }
+    public static void main(String[] args) {
+        write();
+    }
+
+    public static void write(){
+        File f = new File("/Users/anastasiakozlova/Desktop/Koch-App/koch-app/app/src/main/resources/"+LISTE);
+        try {//result is ignored because the existence of the file matters only
+            f.createNewFile();
+            System.out.print("Datei wurde erstellt.");
+        } catch (IOException e) {
+            System.err.print("Datei konnte nicht erstellt werden.");
+            throw new RuntimeException(e);
+        }
+        try(
+                BufferedWriter writer = new BufferedWriter(new FileWriter(f));
+                CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT
+                    .withHeader("Item", "Menge"));) {
+            System.out.print("Daten wurden erstellt.");
+            csvPrinter.printRecord("Gurke", "2");
+            csvPrinter.printRecord("Tomaten", "3");
+            csvPrinter.printRecord("Käse", "1");
+            // Hier speichern lassen
         }
         catch (IOException e) {
             e.printStackTrace();
         }
     }
+
 
 }
 
