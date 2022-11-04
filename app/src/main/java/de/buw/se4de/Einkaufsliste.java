511e003bdb2950d4a31a32583f79faa596ef7eea
Anastasia Koslova <anastasia.koslova@uni-weimar.de>
2022-11-04 11:46:37 +0100

    Versuch II Java zu verstehen
    Ich versuche erst mal, etwas in eine csv file zu schreiben (für die Einkaufsliste)

---
+++ app/src/main/java/de/buw/se4de/Einkaufsliste.java
@@ -0,0 +1,14 @@
+package de.buw.se4de;
+import java.io.*;
+
+public class Einkaufsliste {
+    String[][] employees = {
+            {"Man", "Sparkes", "msparkes0@springhow.com", "Engineering"},
+            {"Dulcinea", "Terzi", "dterzi1@springhow.com", "Engineering"},
+            {"Tamar", "Bedder", "tbedder2@springhow.com", "Legal"},
+            {"Vance", "Scouller", "vscouller3@springhow.com", "Sales"},
+            {"Gran", "Jagoe", "gjagoe4@springhow.com", "Business Development"}
+    };
+    File csvFile = new File("employees.csv");
+}
+
