fa9a31fc45ac99e576b8c9768ec6a681ca79f13c
Anastasia Koslova <anastasia.koslova@uni-weimar.de>
2022-11-04 12:16:38 +0100

    Erster Ansatz der Einkaufsliste

--- app/src/main/java/de/buw/se4de/Einkaufsliste.java
+++ app/src/main/java/de/buw/se4de/Einkaufsliste.java
@@ -1,14 +1,31 @@
 package de.buw.se4de;
-import java.io.*;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 
 public class Einkaufsliste {
-    String[][] employees = {
-            {"Man", "Sparkes", "msparkes0@springhow.com", "Engineering"},
-            {"Dulcinea", "Terzi", "dterzi1@springhow.com", "Engineering"},
-            {"Tamar", "Bedder", "tbedder2@springhow.com", "Legal"},
-            {"Vance", "Scouller", "vscouller3@springhow.com", "Sales"},
-            {"Gran", "Jagoe", "gjagoe4@springhow.com", "Business Development"}
-    };
-    File csvFile = new File("employees.csv");
+    String item;
+
+    public static void main (String[] args){
+        String line = "";
+        String splitBy = ",";
+        try {
+            BufferedReader a = new BufferedReader(new FileReader("liste.csv"));
+            while ((line = a.readLine()) != null)   //returns a Boolean value
+            {
+                System.out.println(line);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
 
