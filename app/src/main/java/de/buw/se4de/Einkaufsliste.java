305c83206d39f0d12416ce3787dcbca9e1b05935
Anastasia Koslova <anastasia.koslova@uni-weimar.de>
2022-11-09 14:21:14 +0100

    Almost ready

--- app/src/main/java/de/buw/se4de/Einkaufsliste.java
+++ app/src/main/java/de/buw/se4de/Einkaufsliste.java
@@ -6,9 +6,11 @@ import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.Scanner;
 
-import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Einkaufsliste {
     private static final String LISTE = "liste.csv";
@@ -17,16 +19,21 @@ public class Einkaufsliste {
         load();
         write();
         load();
+        delete();
+        load();
     }
 
     public static void load() {
         File f = new File("src/main/resources/"+LISTE);
+        //opening the reader and parser for the csv file
         try ( BufferedReader reader = new BufferedReader(new FileReader(f));
         CSVParser csvParser = CSVParser.parse(reader, CSVFormat.DEFAULT) ){
-        for (CSVRecord csvRecord : csvParser) {
-            String item = csvRecord.get(0);
-            String menge = csvRecord.get(1);
-            System.out.println(item + " " + menge);
+            //looping through the lines in the csv file
+            for (CSVRecord csvRecord : csvParser) {
+                String item = csvRecord.get(0);
+                String menge = csvRecord.get(1);
+                //printing each line
+                System.out.println(item + " " + menge);
         }
         }
         catch (IOException e) {
@@ -35,50 +42,31 @@ public class Einkaufsliste {
     }
     public static void write(){
         File f = new File("/Users/anastasiakozlova/Desktop/Koch-App/koch-app/app/src/main/resources/"+LISTE);
+        //checking if this file is already there, otherwise error
         try {//result is ignored because the existence of the file matters only
             f.createNewFile();
         } catch (IOException e) {
-            System.err.print("Datei konnte nicht erstellt werden.");
+            System.err.print("File could not be created.");
             throw new RuntimeException(e);
         }
-        /*try(
-                FileWriter writer = new FileWriter(f);) {
-            Scanner object = new Scanner(System.in);
-            String item;
-            do {
-                System.out.println("Was möchtest du zur Einkaufsliste hinzufügen? (Wenn nichts, dann schreib 'nichts')");
-                item = object.nextLine();
-                if (item.equalsIgnoreCase("nichts")) { break; }
-                System.out.println("Wie viel möchtest du davon hinzufügen?");
-                String menge = object.nextLine();
-                String line = item + "," + menge + "\n";
-                writer.write(line);
-                // Hier muss noch gespeichert werden
-            } while (true);
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }*/
+        //opening the writer to write into the csv file
         try(
-                //BufferedWriter writer = new BufferedWriter(new FileWriter(f));
                 BufferedWriter writer = new BufferedWriter(new FileWriter(f, true));
-                CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT);
-                //CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT
-                //   .withHeader("Item", "Menge"));) {
-                ){
+                CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT)){
+            //opening the scanner for user's input
             Scanner object = new Scanner(System.in);
-            String item;
             do {
-                //StringBuilder sb = new StringBuilder();
-                System.out.println("Was möchtest du zur Einkaufsliste hinzufügen? (Wenn nichts, dann schreib 'nichts')");
-                item = object.nextLine();
-                if (item.equalsIgnoreCase("nichts")) { break; }
-                System.out.println("Wie viel möchtest du davon hinzufügen?");
+                //asking the user for input
+                System.out.println("Would you like to add something to your grocery list? (If no, then type 'no')");
+                //scanning his answer
+                String item = object.nextLine();
+                //breaking the while loop if the user does not want to add any items
+                if (item.equalsIgnoreCase("no")) { break; }
+                //asking the user how much he wants to add of the item
+                System.out.println("How much of it would you like to add?");
                 String menge = object.nextLine();
-                //String i = item + "," + menge + "\n";
-                //writer.append(i);
+                //recording those values in the csv file
                 csvPrinter.printRecord(item, menge);
-                // Hier muss noch gespeichert werden
             } while (true);
         }
         catch (IOException e) {
@@ -87,15 +75,46 @@ public class Einkaufsliste {
     }
     public static void delete() {
         File f = new File("src/main/resources/"+LISTE);
+        //creates array with all ingredients to rewrite the file after deleting an item
+        List<String> list = new ArrayList<>();
+        //reads liste.csv
         try ( BufferedReader reader = new BufferedReader(new FileReader(f));
-              CSVParser csvParser = CSVParser.parse(reader, CSVFormat.DEFAULT) ){
+              CSVParser csvParser = CSVParser.parse(reader, CSVFormat.DEFAULT)){
+            //creates scanner for user's input
             Scanner object = new Scanner(System.in);
-            String item;
+            //starts the option to 'delete' an item
             do {
-                System.out.println("Möchtest du etwas löschen? (Wenn nein, dann schreib 'nein')");
-                item = object.nextLine();
-                if (item.equalsIgnoreCase("nein")) { break; }
-                // here you'll delete
+                //Scanning user's input for deleting an item
+                System.out.println("Would you like to delete something? (If no, then type 'no')");
+                String item = object.nextLine();
+                //breaks while loop if nothing should be deleted
+                if (item.equalsIgnoreCase("no")) { break; }
+                //adds all items and amounts to the array
+                for (CSVRecord csvRecord : csvParser) {
+                    list.addAll(Arrays.asList(csvRecord.get(0), csvRecord.get(1)));
+                }
+                //case if item is not on the list
+                if (!list.contains(item)) {
+                    System.out.println("This item is not on your list.");
+                }
+                else {
+                    //starting to overwrite the file
+                    try (FileWriter writer = new FileWriter(f)) {
+                        //all items except deleted one will be written into csv file
+                        for (int i = 0; i < list.size() / 2; i++) {
+                            String item2 = list.get(2 * i);
+                            //if item2 is not users item, then write them into the csv file
+                            if (!item2.equalsIgnoreCase(item)) {
+                                String line = item2 + "," + list.get(2 * i + 1) + "\n";
+                                writer.write(line);
+                            }
+                        }
+                        //removes the item and amount from the array
+                        int k = list.indexOf(item);
+                        list.remove(list.get(k));
+                        list.remove(list.get(k+1));
+                    }
+                }
             } while (true);
             }
         catch (IOException e) {
