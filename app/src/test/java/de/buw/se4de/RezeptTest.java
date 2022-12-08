5bd06ac9fc79717e2dbaa4944c82e04c8a69301c
Mariia Ufimtseva <mariia.ufimtseva@uni-weimar.de>
2022-12-08 18:07:56 +0100

    added constructor test for recipes

--- app/src/test/java/de/buw/se4de/RezeptTest.java
+++ app/src/test/java/de/buw/se4de/RezeptTest.java
@@ -22,4 +22,27 @@ class RezeptTest {
 
 		assertNotEquals("", r.ingredientString(false));
 	}
-}
+
+	void testConstructor(){
+		String name = "Eierkuchen";
+		Ingredient eier = new Ingredient(1, "st", "Eier");
+		Ingredient mehl = new Ingredient(150, "g", "Mehl");
+		ArrayList<Ingredient> zutaten = new ArrayList<Ingredient>();
+		zutaten.add(eier);
+		zutaten.add(mehl);
+		String personen = "2";
+		String[] kategorien = null;
+		String zeit = "30 min";
+		String zubereitung = "backen";
+
+		Rezept eierkuchen = new Rezept(name, zutaten, personen, kategorien, zeit, zubereitung);
+
+		assertEquals(name, eierkuchen.name);
+		assertEquals(zutaten, eierkuchen.ingredients);
+		assertEquals(personen, eierkuchen.personen);
+		assertEquals(kategorien, eierkuchen.kategorien);
+		assertEquals("Unbewertet", eierkuchen.rating);
+		assertEquals(zeit, eierkuchen.zeit);
+		assertEquals(zubereitung, eierkuchen.zubereitung);
+		}
+	}
